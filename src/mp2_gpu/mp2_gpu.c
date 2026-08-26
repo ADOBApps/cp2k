036e0d73140c8c341413ea0d12ffebe9a036bebb
ADOBApps <acxelorozco@gmail.com>
2026-08-26 13:53:29 +0200

    parallel version ij_counter_send forense mark 9

--- src/mp2_gpu/mp2_gpu.c
+++ src/mp2_gpu/mp2_gpu.c
@@ -1140,7 +1140,7 @@ void calc_ri_mp2_energy(
                 if (ij_index <= send_ij_index) {
                     // Calculate send indices for this ij pair
                     // int correction_send = (integ_group_pos2color_sub[proc_send] < 0) ? 1 : 0;
-                    int correction_send = min_int(1, integ_group_pos2color_sub[proc_send]);
+                    int correction_send = min_int(0, integ_group_pos2color_sub[proc_send]);
                     int ij_counter_send = (ij_index - correction_send) * ngroup + integ_group_pos2color_sub[proc_send];
                     // int ij_counter_send = ij_index * ngroup + integ_group_pos2color_sub[proc_send];
 
