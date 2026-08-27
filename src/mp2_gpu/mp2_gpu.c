a44c12a0a1a319d9c24c59fe1633c5faa8c9adc0
ADOBApps <acxelorozco@gmail.com>
2026-08-27 02:21:37 +0200

    parallel version ij_counter_send forense mark 16

--- src/mp2_gpu/mp2_gpu.c
+++ src/mp2_gpu/mp2_gpu.c
@@ -1140,11 +1140,12 @@ void calc_ri_mp2_energy(
                 if (ij_index <= send_ij_index) {
                     // Calculate send indices for this ij pair
                     // int correction_send = (integ_group_pos2color_sub[proc_send] > 0) ? 1 : 0;
-                    int correction_send = min_int(1, integ_group_pos2color_sub[proc_send]);
-                    // int correction_send_2 = (ij_index <= 0) ? ij_index : (ij_index - correction_send);
-                    int ij_counter_send = (ij_index - correction_send) * ngroup + integ_group_pos2color_sub[proc_send];
+                    // int correction_send = min_int(1, integ_group_pos2color_sub[proc_send]);
+                    // int correction_send_2 = (ij_index <= 0) ? ij_index : (ij_index - correction_send); 
+                    // int ij_counter_send = (correction_send_2) * ngroup + integ_group_pos2color_sub[proc_send];
+                    // int ij_counter_send = (ij_index - correction_send) * ngroup + integ_group_pos2color_sub[proc_send];
                     // Original
-                    // int ij_counter_send = ij_index * ngroup + integ_group_pos2color_sub[proc_send];
+                    int ij_counter_send = ij_index * ngroup + integ_group_pos2color_sub[proc_send];
 
                     // Assert bounds
                     // printf("correction_send: %d\n\n", correction_send);
@@ -1512,7 +1513,8 @@ void calc_ri_mp2_energy(
                     // Calculate send indices for this ij pair
                     // int ij_counter_send = (ij_index - 1) * ngroup + integ_group_pos2color_sub[proc_send];
                     int correction_send = (integ_group_pos2color_sub[proc_send] > 0) ? 1 : 0;
-                    int ij_counter_send = (ij_index - correction_send) * ngroup + integ_group_pos2color_sub[proc_send];
+                    // int ij_counter_send = (ij_index - correction_send) * ngroup + integ_group_pos2color_sub[proc_send];
+                    int ij_counter_send = (ij_index) * ngroup + integ_group_pos2color_sub[proc_send];
                     assert(ij_counter_send >= 0 && ij_counter_send < total_ij_pairs_blocks);
 
                     // int send_i = ij_map[0 * total_ij_pairs + ij_counter_send - 1];
